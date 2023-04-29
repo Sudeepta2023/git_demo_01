7a183d498c8d1c35b7aad4242be591a732300744
Sudeepta Kumar Pradhan <od0346sudeepta@gmail.com>
2023-04-30 00:47:38 +0530

    commit

--- Hello.js
+++ Hello.js
@@ -3,8 +3,4 @@ console.log("hiiii")
 console.log("Creating a new branch");
 console.log("another message");
 console.log("another  new message");
-<<<<<<< HEAD
-console.log("create feature branch1 ")
-=======
-console.log("main branc change by user ");
->>>>>>> 7dfa5b89c7f7686ab3f073db457d2ec07a9dfb80
+
