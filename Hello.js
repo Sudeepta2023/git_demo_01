fce56d6cff7b7b546200da754bffb1b642b8f4f2
Sudeepta Kumar Pradhan <od0346sudeepta@gmail.com>
2023-04-29 22:48:46 +0530

    arthimatic operation

--- Hello.js
+++ Hello.js
@@ -2,3 +2,4 @@ console.log("Hi sudeepta happy to  see you again");
 console.log("hiiii")
 console.log("Creating a new branch");
 console.log("another message");
+console.log("another  new message");
---
+++ artm.js
@@ -0,0 +1,4 @@
+var a=65;
+var p=36;
+var sum = a+p;
+console.log("sum of two number is - " + sum)
